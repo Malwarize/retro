95d4ba82f1805e1f1cdadfd31d9e333379b827cf
xorbit <orbit127001@gmail.com>
2024-03-07 21:10:50 +0200

    add update readme script

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -6,8 +6,15 @@
 # regex in the README.md to the new version
 
 version=$1
+if [ -z "$version" ]; then
+  echo "version is required"
+  exit 1
+fi
+
 sed -i "s/v[0-9]\.[0-9]\.[0-9]/v$version/" README.md
 git add README.md
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "github-actions[bot]"
 git commit -m "bump version to $version"
 git push origin main
 
