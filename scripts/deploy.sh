05e5c3fb4e20deefe8960e6f547ac0177fedc0dd
xorbit <orbit127001@gmail.com>
2024-03-07 20:59:46 +0200

    add update readme script

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# author: XORbit01@protonmail
+#
+# this script is used locally to deploy the app to the server 
+# get the latest tag then increment it by 1 
+# then push the new tag to the repo 
+#get the latest tag name from remote
+
+git fetch --tags
+latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "latest tag is $latest_tag"
+
+# then increment it
+new_tag=$(echo "$latest_tag" | awk -F. -v OFS=. '{$NF++;print}')
+echo "new tag is $new_tag"
+
+git commit -m "bump version to $new_tag"
+git push origin main
+
+# create a tag
+git tag "$new_tag"
+git push origin "$new_tag"
